629a435c772d106dc6ed4246934674b1f9ce0f5b
Michal Krenek (Mikos) <m.krenek@gmail.com>
2015-04-15 15:08:25 +0200

    Change text_to_segments() function to use new parser

--- hangupsbot/utils.py
+++ hangupsbot/utils.py
@@ -2,25 +2,12 @@ import unicodedata, string
 
 import hangups
 
+from hangupsbot.parser import ChatMessageParser, tokens
 
 def text_to_segments(text):
     """Create list of message segments from text"""
-    # Replace two consecutive spaces with space and non-breakable space,
-    # then split text to lines
-    lines = text.replace('  ', ' \xa0').splitlines()
-    if not lines:
-        return []
-
-    # Generate line segments
-    segments = []
-    for line in lines[:-1]:
-        if line:
-            segments.append(hangups.ChatMessageSegment(line))
-        segments.append(hangups.ChatMessageSegment('\n', hangups.SegmentType.LINE_BREAK))
-    if lines[-1]:
-        segments.append(hangups.ChatMessageSegment(lines[-1]))
-
-    return segments
+    parser = ChatMessageParser(tokens)
+    return parser.parse(text)
 
 
 def unicode_to_ascii(text):
